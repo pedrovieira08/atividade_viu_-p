f011220a95aab786d086dca0f3eb651ef4dac4f2
pedrovieira08 <fafnir.pgv@gmail.com>
2025-04-15 16:58:21 -0300

    opa

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,90 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <body>
+    <header>
+      <div>
+        <h1>IFbooks</h1>
+      <p>
+        Apreço a <br>
+        leitura
+      </p>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Termos</a>
+          </li>
+          <li>
+            <a href="#">Equpe</a>
+          </li>
+          <li>
+            <a href="#">Envio</a>
+          </li>
+          <li>
+            <a href="#">Devoluções</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section class="book">
+        <div>
+          <p class="autor">Autor de Abril</p>
+          <h2>Eric-Emanuel Schmitt </h2>
+          <p>Eric-Emmanuel Schmitt has been awarded more than 20 literary prizes and distinctions, and in 2001 he received the title of Chevalier des Arts et des Lettres. His books have been translated into over 40 languages.</p>
+          <button>Acessar página do livro</button>
+        </div>
+        
+      </section>
+    </main>
+  </body>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
-
 <style scoped>
-header {
-  line-height: 1.5;
+a{
+    color: black;
+    text-decoration: none;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+/*==================================
+               topo
+====================================*/
+header{
+  display: flex;
+  justify-content:space-between;
+  align-items: center;
+  margin: 15px 0px 15px 0px;
+  
+}header div h1{
+  font-size: 2rem;
 }
+header nav ul{
+  display: flex;
+  align-items: center; 
+  list-style: none;
+}header div{
+  display: flex;
+}header div p{
+  color: #27AE60;
+  text-align: center;
+  font-size: larger;
+}
+header nav ul li a{
+  padding: 20px;
+  justify-content: space-between;
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+}
+/* book */
+section.book{
+ padding: 180px;
+  border-top: solid 2px;
+  border-bottom: solid 2px;
+  
+}section.book div p.autor{
+  border: solid 2px;
+  border-radius: 60px;
+  margin: 10px;
+  padding: 10px;
 }
 </style>
